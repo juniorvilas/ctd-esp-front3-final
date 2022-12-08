fd654aa3747d704f9c4436f45ada73cfd9fa67df
juniorvilas <log_junior@hotmail.com>
2022-12-07 22:16:47 -0300

    feat: add new tipe formdata

--- shared/types/apiSchema.ts
+++ shared/types/apiSchema.ts
@@ -1,3 +1,19 @@
+export type FormData = {
+    name: string;
+    lastname: string;
+    email: string;
+    address1: string;
+    address2?: string | null;
+    city: string;
+    state: string;
+    zipCode: string;
+    number: string;
+    cvc: string;
+    expDate: string;
+    nameOnCard: string;
+}
+
+
 export type Craracter = {
     id: number;
     name: string
