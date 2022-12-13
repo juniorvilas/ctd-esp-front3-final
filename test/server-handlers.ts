2cec3c64d1c4bcdb2ff3689264183eb6848b5c3e
juniorvilas <log_junior@hotmail.com>
2022-12-13 20:46:14 -0300

    refactor: another modification

--- test/server-handlers.ts
+++ test/server-handlers.ts
@@ -1,7 +1,7 @@
 import { rest } from "msw";
 import comics from "dh-marvel/test/mocks/comics";
-import character from "dh-marvel/test/mocks/character";
-import comic from "dh-marvel/test/mocks/comic";
+import character from "dh-marvel/test/mocks/character.json";
+import comic from "dh-marvel/test/mocks/comic.json";
 import comicsWithOffsetAndLimit from "dh-marvel/test/mocks/comicsWithOffsetAndLimit";
 import comicWithoutStock from "dh-marvel/test/mocks/comicWithoutStock";
 
