88b95bdecf46f39b0ffb1647b5612d47d142ae41
juniorvilas <log_junior@hotmail.com>
2022-12-12 21:54:52 -0300

    add queryclient to index test

--- pages/index.spec.tsx
+++ pages/index.spec.tsx
@@ -1,13 +1,32 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import IndexPage from "dh-marvel/pages/index.page";
 import Index from "dh-marvel/pages/index.page";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+
 
 describe("IndexPage", () => {
   describe("when rendering default", () => {
-    it("should render the title", () => {
-      render(<Index />);
-      const title = screen.getByText("Sample");
-      expect(title).toBeInTheDocument();
-    });
+    it("should render the title",
+      async () => {
+        render(
+          <QueryClientProvider client={queryClient}>
+            <Index />
+          </QueryClientProvider>
+        );
+
+        const title = screen.getByText(/Seja bem vindo ao DH Marvel/i);
+
+
+        await waitFor(() => {
+          expect(title).toBeInTheDocument()
+        });
+
+
+
+      });
   });
 });
