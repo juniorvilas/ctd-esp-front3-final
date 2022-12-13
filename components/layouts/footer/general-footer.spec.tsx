966367f7f75f14e392fd220f579e2f3586b4249c
juniorvilas <log_junior@hotmail.com>
2022-12-12 21:54:01 -0300

    refactor: change text from test footer

--- components/layouts/footer/general-footer.spec.tsx
+++ components/layouts/footer/general-footer.spec.tsx
@@ -5,12 +5,12 @@ describe("GeneralFooter", () => {
   describe("when rendering default layout", () => {
     it("should render the powered by text", () => {
       render(<GeneralFooter />);
-      const poweredBy = screen.getByText("Powered by");
+      const poweredBy = screen.getByText(/Powered by/i);
       expect(poweredBy).toBeInTheDocument();
     });
     it("should render the logo", () => {
       render(<GeneralFooter />);
-      const logo = screen.getByAltText("Digital House Logo");
+      const logo = screen.getByText(/Carlindo Junior/i);
       expect(logo).toBeInTheDocument();
     });
   });
